14a23cc2660857f7a7f710a7d10c14ada6fe0916
William Chen <william@terran.one>
2023-11-20 22:11:29 -0800

    Refactor code to add support for rendering function definitions.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,128 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import * as CG from './codegen';
+
+export interface Render {
+  render(): string;
+}
+
+export type Stmt = UseStmt | ConstStmt | EnumDefn | ModuleDefn;
+export class Module implements Render {
+  constructor(
+    public name: string,
+    public body: Stmt[]
+  ) {}
+
+  public render() {
+    return `pub mod ${this.name} {
+      ${this.body.map((statement) => statement.render()).join('\n')}
+    }`;
+  }
+}
+
+export class UseStmt implements Render {
+  constructor(public path: string) {}
+
+  public render() {
+    return `use ${this.path};`;
+  }
+}
+
+export class ConstStmt implements Render {
+  constructor(
+    public name: string,
+    public ty: string,
+    public value: Render
+  ) {}
+
+  public render() {
+    return `const ${this.name}: ${this.ty} = ${this.value.render()};`;
+  }
+}
+
+export class EnumDefn implements Render {
+  constructor(
+    public name: string,
+    public variants: EnumVariant[]
+  ) {}
+
+  public render() {
+    return `pub enum ${this.name} {
+      ${this.variants.map((variant) => variant.render()).join(',\n')}
+    }`;
+  }
+}
+
+export type EnumVariant =
+  | EnumVariantStruct
+  | EnumVariantTuple
+  | EnumVariantUnit;
+
+export class EnumVariantStruct implements Render {
+  constructor(
+    public name: string,
+    public fields: StructField[]
+  ) {}
+
+  public render() {
+    return `${this.name} {
+      ${this.fields.map((field) => field.render()).join(',\n')}
+    }`;
+  }
+}
+
+export class EnumVariantTuple implements Render {
+  constructor(
+    public name: string,
+    public fields: string[]
+  ) {}
+
+  public render() {
+    return `${this.name}(${this.fields.join(', ')})`;
+  }
+}
+
+export class EnumVariantUnit implements Render {
+  constructor(public name: string) {}
+
+  public render() {
+    return `${this.name}`;
+  }
+}
+
+export class StructField implements Render {
+  constructor(
+    public name: string,
+    public type: string
+  ) {}
+
+  public render() {
+    return `${this.name}: ${this.type}`;
+  }
+}
+
+export class FunctionParam implements Render {
+  constructor(
+    public name: string,
+    public type: string
+  ) {}
+
+  public render() {
+    return `${this.name}: ${this.type}`;
+  }
+}
+
+export class FunctionDefn implements Render {
+  constructor(
+    public name: string,
+    public params: FunctionParam[],
+    public returnType: string,
+    public body: Render[]
+  ) {}
+
+  public render() {
+    return `pub fn ${this.name}(${this.params
+      .map((param) => param.render())
+      .join(', ')}): ${this.returnType} {
+      ${this.body.map((statement) => statement.render()).join('\n')}
+    }`;
+  }
 }
