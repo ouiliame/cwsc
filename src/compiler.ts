a6e3f99fd914563765c9596915eca4e6294a3c22
William Chen <william@terran.one>
2023-11-23 09:09:39 -0800

    ad compiler

--- src/compiler.ts
+++ src/compiler.ts
@@ -1,3 +1,48 @@
+import * as fs from 'fs';
+import { FsProjectManager } from './projects';
+import { CWScriptParser } from './parser';
+
+/**
+ * The CWScript compiler.
+ */
+
+export type CompilationCtx = {
+  projectRoot: string;
+  start: number;
+  end?: number;
+  sources: {
+    [sourceFile: string]: {
+      ast: any;
+      text: string;
+      diagnostics: any[];
+    };
+  };
+};
+
 export class CWScriptCompiler {
-  constructor() {}
+  constructor(public projectManager: FsProjectManager) {}
+
+  /**
+   * Builds all compilation targets within the project.
+   */
+  public build() {
+    // Create new compilation context
+    let ctx: CompilationCtx = {
+      projectRoot: this.projectManager.projectRoot,
+      start: Date.now(),
+      sources: {},
+    };
+
+    // Parse project source files
+    this.projectManager.sourceFiles.forEach((sourceFile) => {
+      const sourceText = fs.readFileSync(sourceFile, 'utf8');
+      const parser = new CWScriptParser(sourceText, sourceFile);
+      const ast = parser.parse();
+      ctx.sources[sourceFile] = {
+        ast,
+        text: sourceText,
+        diagnostics: [],
+      };
+    });
+  }
 }
