d9d2e82b2064b0aebd5274bb4b960b3385566c7b
William Chen <william@terran.one>
2023-11-23 23:37:21 -0800

    compiler

--- src/compiler.ts
+++ src/compiler.ts
@@ -8,8 +8,6 @@ import { CWScriptParser } from './parser';
 
 export type CompilationCtx = {
   projectRoot: string;
-  start: number;
-  end?: number;
   sources: {
     [sourceFile: string]: {
       ast: any;
@@ -26,23 +24,42 @@ export class CWScriptCompiler {
    * Builds all compilation targets within the project.
    */
   public build() {
-    // Create new compilation context
-    let ctx: CompilationCtx = {
-      projectRoot: this.projectManager.projectRoot,
-      start: Date.now(),
-      sources: {},
-    };
-
-    // Parse project source files
-    this.projectManager.sourceFiles.forEach((sourceFile) => {
-      const sourceText = fs.readFileSync(sourceFile, 'utf8');
-      const parser = new CWScriptParser(sourceText, sourceFile);
-      const ast = parser.parse();
-      ctx.sources[sourceFile] = {
-        ast,
-        text: sourceText,
-        diagnostics: [],
-      };
+    // 1. Create a fresh compilation context
+    const ctx = this.createCtx({
+      project: this.projectManager.project,
     });
-  }
+
+    // 2. Populate context with project config, compiler options, build options
+    ctx.project = this.projectManager.project;
+
+    // 3. Get the list of source files and read file contents
+    const sourceFiles = this.projectManager.sourceFiles;
+
+    sourceFiles.forEach((sourceFile) => {
+      // read file contents
+      const sourceText = fs.readFileSync(sourceFile, 'utf8');
+      ctx.sources[sourceFile].text = sourceText;
+    });
+
+    // 4. Parse each source file into AST (SourceFile)
+    ctx.sources.forEach((sourceFile) => {
+      ctx.sources[sourceFile].ast = this.parse(sourceFile);
+    });
+
+    // 5. Create symbol table for this compilation context
+    ctx.createSymbolTable();
+
+    // 6. Convert AST to ISR
+    ctx.convertAstToIr();
+
+    // 7. Perform semantic analysis passes
+    ctx.performSemanticPasses();
+  
+    // 9. For each build target, generate code
+    ctx.buildTargets();
+
 }
+
+const pm = new FsProjectManager('./examples/counter');
+const compiler = new CWScriptCompiler(pm);
+compiler.build();
